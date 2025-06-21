7712ebf8a7677215adb5332c74e13308d6309fc6
Gerhard Tackey <tackeygerhard@gmail.com>
2025-06-21 09:15:53 -0700

    fix: Correctly track backend folder contents

---
+++ backend/src/main/java/com/qwadwocodes/konvo/dto/UserDto.java
@@ -0,0 +1,15 @@
+package com.qwadwocodes.konvo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    private Long id;
+    private String firstName;
+    private String phoneNumber;
+    private String profilePictureUrl;
+} 
